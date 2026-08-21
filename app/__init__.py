8f7b7fce741d3746752801aa38c2157e56eefa5e
rdiazcabal <40671887+rdiazcabal@users.noreply.github.com>
2026-08-21 11:23:06 -0600

    Install Spanish analysis presentation patch

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 """SecuRI application package."""
 
 from app.license_model import install_license_model_patch
+from app.spanish_analysis_patch import install_spanish_analysis_patch
 
 install_license_model_patch()
+install_spanish_analysis_patch()

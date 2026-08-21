c4cd2903a4d20d225158f86d8146b00239528a59
rdiazcabal <40671887+rdiazcabal@users.noreply.github.com>
2026-08-21 11:33:54 -0600

    Load country name normalization patch

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@
 
 from app.license_model import install_license_model_patch
 from app.spanish_analysis_patch import install_spanish_analysis_patch
+from app.country_name_patch import install_country_name_patch
 
 install_license_model_patch()
 install_spanish_analysis_patch()
+install_country_name_patch()
